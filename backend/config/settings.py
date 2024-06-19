60b63b2da6d7c150a359c7d3d35fa212188bfe73
rednek21 <egor.podustov@mail.ru>
2024-06-19 13:02:22 +0300

    add reset password error setting

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -221,10 +221,15 @@ REST_FRAMEWORK = {
 # DJOSER
 
 DJOSER = {
+    'PASSWORD_RESET_SHOW_EMAIL_NOT_FOUND': True,
     'PASSWORD_RESET_CONFIRM_URL': '/password/reset/confirm/{user}/{uid}/{token}',
+
     'USERNAME_RESET_CONFIRM_URL': '/username/reset/confirm/{user}/{uid}/{token}',
+
     'ACTIVATION_URL': '/activate/{user}/{uid}/{token}',
+
     'SEND_ACTIVATION_EMAIL': True,
+
     'SERIALIZERS': {
         'users': 'users.serializers.UserSerializer',
     },
@@ -232,7 +237,8 @@ DJOSER = {
         'activation': 'mailings.views.UserActivationEmail',
         'password_reset': 'mailings.views.UserPasswordResetEmail',
         'password_changed_confirmation': 'djoser.email.PasswordChangedConfirmationEmail',
-    }
+    },
+
 }
 
 # JWT
