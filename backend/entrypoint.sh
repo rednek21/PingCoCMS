c1944aecee6ee0e1c4e6a79f4966f2f6dce1bfe1
rednek21 <egor.podustov@mail.ru>
2024-06-29 15:34:43 +0300

    db waiting removed from entrypoint

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -1,27 +1,11 @@
 #!/bin/sh
 
-if [ "$DATABASE" = "postgres" ]
-then
-    echo "Waiting for postgres..."
-
-    while ! nc -z $DATABASE_HOST $DATABASE_PORT; do
-      sleep 0.1
-    done
-
-    echo "PostgreSQL started"
-fi
-
 python manage.py flush --no-input
 python manage.py migrate
 
 if [ "$DEBUG" = "False" ]
 then
     python manage.py collectstatic --noinput
-    python manage.py seed pages --number=5
-    python manage.py seed users --number=5
-else
-    python manage.py seed pages --number=5
-    python manage.py seed users --number=5
 fi
 
 if [ "$DJANGO_SUPERUSER_USERNAME" ]
@@ -31,5 +15,7 @@ then
         --username $DJANGO_SUPERUSER_USERNAME \
         --email $DJANGO_SUPERUSER_EMAIL
 fi
+
+gunicorn config.wsgi:application --bind 0.0.0.0:8000
 
 exec "$@"
